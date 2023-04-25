879a932f9166db3578a67e265b06318f8c88a4d0
Igor L <ilupats@yahoo.com>
2023-04-25 12:02:49 +0300

    first commit

---
+++ gamelet.js
@@ -0,0 +1,22 @@
+const ball = document.getElementById('ball');
+document.addEventListener('keydown', handleKeyPress);
+let position = 0;
+
+function handleKeyPress(e) {
+  if (e.code === 'KeyQ') {
+    position = position - 100;
+  }
+  if (e.code === 'KeyE') {
+    position = position + 100;
+  }
+  
+  if (position < 0) {
+    position = 0;
+  }
+  refresh();
+}
+function refresh() {
+  ball.style.left = position + 'px';
+}
+
+	    
